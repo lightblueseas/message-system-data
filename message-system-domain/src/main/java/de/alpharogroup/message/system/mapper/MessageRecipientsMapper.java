07c706274f142de2130a67c5e4718d7c3cad920f
astrapi69 <asterios.raptis@gmx.net>
2015-12-12 14:12:38 +0100

    Mapper updated after refactoring.

--- message-system-domain/src/main/java/de/alpharogroup/message/system/mapper/MessageRecipientsMapper.java
+++ message-system-domain/src/main/java/de/alpharogroup/message/system/mapper/MessageRecipientsMapper.java
@@ -1,9 +1,9 @@
 package de.alpharogroup.message.system.mapper;
 
-import de.alpharogroup.db.entitymapper.AbstractEntityBOMapper;
+import de.alpharogroup.db.entitymapper.AbstractEntityDOMapper;
 import de.alpharogroup.message.system.entities.MessageRecipients;
 import de.alpharogroup.message.system.domain.MessageRecipient;
 
-public class MessageRecipientsMapper extends AbstractEntityBOMapper<MessageRecipients, MessageRecipient> {
+public class MessageRecipientsMapper extends AbstractEntityDOMapper<MessageRecipients, MessageRecipient> {
 
 }
--- message-system-domain/src/main/java/de/alpharogroup/message/system/mapper/MessagesMapper.java
+++ message-system-domain/src/main/java/de/alpharogroup/message/system/mapper/MessagesMapper.java
@@ -1,9 +1,9 @@
 package de.alpharogroup.message.system.mapper;
 
-import de.alpharogroup.db.entitymapper.AbstractEntityBOMapper;
+import de.alpharogroup.db.entitymapper.AbstractEntityDOMapper;
 import de.alpharogroup.message.system.domain.Message;
 import de.alpharogroup.message.system.entities.Messages;
 
-public class MessagesMapper extends AbstractEntityBOMapper<Messages, Message> {
+public class MessagesMapper extends AbstractEntityDOMapper<Messages, Message> {
 
 }
