c319a77f81b5040ea111106ffb7a66c9d88e12d1
astrapi69 <asterios.raptis@gmx.net>
2016-01-02 14:18:25 +0100

    overload of sendMail method with different arguments.

--- message-system-business/src/main/java/de/alpharogroup/message/system/service/SendMessageBusinessService.java
+++ message-system-business/src/main/java/de/alpharogroup/message/system/service/SendMessageBusinessService.java
@@ -30,7 +30,7 @@ public class SendMessageBusinessService {
 	 * @throws MessagingException the messaging exception
 	 */
 	public static void sendInfoEmail(final SendEmail emailSender,
-			InfoMessageModel model) throws MessagingException {
+			final InfoMessageModel model) throws MessagingException {
 		sendInfoEmail(emailSender, model, null);
 	}
 
@@ -43,18 +43,37 @@ public class SendMessageBusinessService {
 	 * @throws MessagingException the messaging exception
 	 */
 	public static void sendInfoEmail(final SendEmail emailSender,
-			InfoMessageModel model, Map<String, String> emailHeaders) throws MessagingException {
-		sendEmail(emailSender, 
-				model.getApplicationSenderAddress(), 
-				model.getApplicationDomainName(), 
-				model.getRecipientEmailContact(), 
-				model.getRecipientFullName(), 
-				model.getMessageContentModel().getSubject(), 
+			final InfoMessageModel model, final Map<String, String> emailHeaders) throws MessagingException {
+		sendEmail(emailSender,
+				model.getApplicationSenderAddress(),
+				model.getApplicationDomainName(),
+				model.getRecipientEmailContact(),
+				model.getRecipientFullName(),
+				model.getMessageContentModel().getSubject(),
 				model.getMessageContentModel().getContent(), emailHeaders);
 	}
 
 	/**
-	 * Send email.
+	 * Send email with default email headers.
+	 *
+	 * @param emailSender the email sender
+	 * @param senderEmail the sender email
+	 * @param senderPersonal the sender personal
+	 * @param recipientEmail the recipient email
+	 * @param recipientPersonal the recipient personal
+	 * @param subject the subject
+	 * @param content the content
+	 * @throws MessagingException the messaging exception
+	 */
+	public static void sendEmail(final SendEmail emailSender, final String senderEmail,
+			final String senderPersonal, final String recipientEmail,
+			final String recipientPersonal, final String subject, final String content)
+			throws MessagingException {
+		sendEmail(emailSender, senderEmail, senderPersonal, recipientEmail, recipientPersonal, subject, content, null);
+	}
+
+	/**
+	 * Send email with the given arguments.
 	 *
 	 * @param emailSender the email sender
 	 * @param senderEmail the sender email
@@ -66,11 +85,37 @@ public class SendMessageBusinessService {
 	 * @param emailHeaders the email headers
 	 * @throws MessagingException the messaging exception
 	 */
-	public static void sendEmail(final SendEmail emailSender, String senderEmail,
-			final String senderPersonal, String recipientEmail,
-			String recipientPersonal, String subject, String content, Map<String, String> emailHeaders)
+	public static void sendEmail(final SendEmail emailSender, final String senderEmail,
+			final String senderPersonal, final String recipientEmail,
+			final String recipientPersonal, final String subject, final String content, final Map<String, String> emailHeaders)
 			throws MessagingException {
-		final EmailMessage emailMessage = new EmailMessage();
+		sendEmail(emailSender, senderEmail, senderPersonal, recipientEmail, recipientPersonal, subject, content, emailHeaders, false);
+	}
+
+	/**
+	 * Send email with the given arguments.
+	 *
+	 * @param emailSender the email sender
+	 * @param senderEmail the sender email
+	 * @param senderPersonal the sender personal
+	 * @param recipientEmail the recipient email
+	 * @param recipientPersonal the recipient personal
+	 * @param subject the subject
+	 * @param content the content
+	 * @param emailHeaders the email headers
+	 * @param withSession the flag if the message will be created with the mail session of the {@link SendEmail}
+	 * @throws MessagingException the messaging exception
+	 */
+	public static void sendEmail(final SendEmail emailSender, final String senderEmail,
+			final String senderPersonal, final String recipientEmail,
+			final String recipientPersonal, String subject, final String content, final Map<String, String> emailHeaders, final boolean withSession)
+			throws MessagingException {
+		final EmailMessage emailMessage;
+		if(withSession) {
+			emailMessage = new EmailMessage(emailSender.getSession());
+		} else {
+			emailMessage = new EmailMessage();
+		}
 		// Set the sender...
 		EmailExtensions.setFromToEmailMessage(senderEmail, senderPersonal,
 				EmailConstants.CHARSET_UTF8, emailMessage);
@@ -79,7 +124,7 @@ public class SendMessageBusinessService {
 				recipientPersonal, EmailConstants.CHARSET_UTF8, emailMessage);
 		// Set subject
 		LOGGER.debug("Subject:"+subject);
-		// Remove new line characters from subject. If the subject contains new line characters a strange behavior occurs... 
+		// Remove new line characters from subject. If the subject contains new line characters a strange behavior occurs...
 		subject = StringExtensions.removeNewlineCharacters(subject);
 		emailMessage.setSubject(subject);
 		// Set content...
@@ -87,7 +132,7 @@ public class SendMessageBusinessService {
 		emailMessage.setUtf8Content(content);
 		// Set email header
 		if(emailHeaders != null && !emailHeaders.isEmpty()) {
-			for (Entry<String, String> emailHeaderEntry : emailHeaders.entrySet()) {
+			for (final Entry<String, String> emailHeaderEntry : emailHeaders.entrySet()) {
 				emailMessage.setHeader(emailHeaderEntry.getKey(), emailHeaderEntry.getValue());
 			}
 		}
