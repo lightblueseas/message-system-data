32ebb8fbab234193daa81f3f39348f8c8a0b4167
Asterios Raptis <asterios.raptis@gmx.net>
2016-07-16 18:29:19 +0200

    Lombokified dao classes

--- message-system-entities/src/main/java/de/alpharogroup/message/system/daos/MessageRecipientsDao.java
+++ message-system-entities/src/main/java/de/alpharogroup/message/system/daos/MessageRecipientsDao.java
@@ -7,25 +7,19 @@ import org.springframework.stereotype.Repository;
 
 import de.alpharogroup.db.dao.jpa.JpaEntityManagerDao;
 import de.alpharogroup.message.system.entities.MessageRecipients;
+import lombok.Getter;
+import lombok.Setter;
 
 @Repository("messageRecipientsDao")
 public class MessageRecipientsDao extends JpaEntityManagerDao<MessageRecipients, Integer>{
-
 	/**
 	 * The serialVersionUID.
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/** The entity manager. */
 	@PersistenceContext
+	@Getter
+	@Setter
 	private EntityManager entityManager;
-
-	@Override
-	public EntityManager getEntityManager() {
-		return entityManager;
-	}
-
-	@Override
-	public void setEntityManager(EntityManager entityManager) {
-		this.entityManager = entityManager;		
-	}
 }
