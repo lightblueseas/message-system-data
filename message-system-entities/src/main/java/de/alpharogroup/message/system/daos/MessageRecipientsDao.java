279d2a051322874e38ce8055d6c66e8222b116ba
Asterios Raptis <asterios.raptis@gmx.net>
2017-03-31 15:31:07 +0200

    update parent version to persistence-api 3.28.0-SNAPSHOT

--- message-system-entities/src/main/java/de/alpharogroup/message/system/daos/MessageRecipientsDao.java
+++ message-system-entities/src/main/java/de/alpharogroup/message/system/daos/MessageRecipientsDao.java
@@ -24,15 +24,10 @@
  */
 package de.alpharogroup.message.system.daos;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.stereotype.Repository;
 
 import de.alpharogroup.db.dao.jpa.JpaEntityManagerDao;
 import de.alpharogroup.message.system.entities.MessageRecipients;
-import lombok.Getter;
-import lombok.Setter;
 
 @Repository("messageRecipientsDao")
 public class MessageRecipientsDao extends JpaEntityManagerDao<MessageRecipients, Integer>{
@@ -40,10 +35,4 @@ public class MessageRecipientsDao extends JpaEntityManagerDao<MessageRecipients,
 	 * The serialVersionUID.
 	 */
 	private static final long serialVersionUID = 1L;
-
-	/** The entity manager. */
-	@PersistenceContext
-	@Getter
-	@Setter
-	private EntityManager entityManager;
 }
--- message-system-entities/src/main/java/de/alpharogroup/message/system/daos/MessagesDao.java
+++ message-system-entities/src/main/java/de/alpharogroup/message/system/daos/MessagesDao.java
@@ -24,31 +24,16 @@
  */
 package de.alpharogroup.message.system.daos;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import de.alpharogroup.db.dao.jpa.JpaEntityManagerDao;
 import de.alpharogroup.message.system.entities.Messages;
-import lombok.Getter;
-import lombok.Setter;
 
 @Repository("messagesDao")
-public class MessagesDao extends JpaEntityManagerDao<Messages, Integer>{
+public class MessagesDao extends JpaEntityManagerDao<Messages, Integer> {
 
-	@Autowired
-	DataSource dataSource;
 	/**
 	 * The serialVersionUID.
 	 */
 	private static final long serialVersionUID = 1L;
-
-	/** The entity manager. */
-	@PersistenceContext
-	@Getter
-	@Setter
-	private EntityManager entityManager;
 }
